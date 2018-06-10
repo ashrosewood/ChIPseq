c51a1b25038f943642a03c456fd7f01a96c7f085
Ashley Woodfin <ashleyrosewoodfin@gmail.com>
2018-06-10 12:39:16 -0500

    ChIP_heatmapMatrix.R updated help

--- bin/ChIP_heatmapMatrix.R
+++ bin/ChIP_heatmapMatrix.R
@@ -2,21 +2,23 @@ args <- commandArgs()
 
 help <- function(){
     cat("ChIP_heatmapMatrix.R :
-- From a provided GRnages obnject make binned heatmap matrix of defined windows around peaks, tss, or tes.
+- From a provided GRnages object make binned heatmap matrix of defined windows around peaks, tss, or tes.
 - For smaller windows 5-10kb, I recommend binning in 25bp. For larger windows, I recommend 50bp bins. 
 - Window is an option if type is Peaks, and upStream and downStream are exclusive to type either Tss or Tes.\n")
  
     cat("Usage: \n")
-    cat("--regions     : GenomicRanges or bed object with the transcripts of interest   [required]\n")
-    cat("--type        : Peaks, Tss, or Tes                                             [required]\n")
-    cat("--window      : total size of region around types above (use for type = Peaks) [default = 5kb]\n")
-    cat("--upStream    : distance upstream of the region to take (use for Tss and Tes)  [default = 2.5kb]\n")
-    cat("--downStream  : distance downstream of the region to take (use for Tss and Tes)[default = 2.5kb]\n")
-    cat("--bins        : number to bin coverage the window should be divisable by this  [default = 25bp]\n")
-    cat("--assembly    : genome assembly build (ex. hg19, dm3)                          [default = hg19]\n")    
-    cat("--bwFile      : path to bigWig File                                            [required]\n")
-    cat("--numCores    : number of cores to use                                         [default = 10 ]\n")
-    cat("--outName     : prefix to your out file names (No .extention)                  [default = basename(bigWigFile) ]\n")
+    cat("--regions     : GenomicRanges or bed object with the transcripts of interest      [required]\n")
+    cat("--type        : Peaks, Tss, or Tes                                                [required]\n")
+    cat("--window      : total size of region around types above (use for type = Peaks)    [default = 5kb]\n")
+    cat("--upStream    : distance upstream of the region to take (use for Tss and Tes)     [default = 2.5kb]\n")
+    cat("--downStream  : distance downstream of the region to take (use for Tss and Tes)   [default = 2.5kb]\n")
+    cat("--bins        : number to bin coverage the window should be divisible by this     [default = 25bp]\n")
+    cat("--assembly    : genome assembly build (ex. hg19, dm3)                             [default = hg19]\n")    
+    cat("--bwFile      : path to bigWig File                                               [required]\n")
+    cat("--numCores    : number of cores to use                                            [default = 10 ]\n")
+    cat("--outName     : prefix to your out file names (No .extension)                     [default = basename(bigWigFile) ]\n")
+    cat("--tssMax      : use the max position (bp) of coverage rather than the start (0/1) [default = 0; use annotated ]
+                           column must be named tssMaxStart and will replace start.                                     \n")
     cat("\n")
     q()
 }
@@ -25,31 +27,26 @@ help <- function(){
 if(length(args)==0 || !is.na(charmatch("-help",args))){
     help()
 } else {
-    regions   <- sub( '--regions=', '', args[grep('--regions=', args)] )
-    Type      <- sub( '--type=', '', args[grep('--type=', args)] )
-    Window    <- sub( '--window=', '', args[grep('--window=', args)] )
+    regions    <- sub( '--regions=', '', args[grep('--regions=', args)] )
+    Type       <- sub( '--type=', '', args[grep('--type=', args)] )
+    Window     <- sub( '--window=', '', args[grep('--window=', args)] )
     upStream   <- sub( '--upStream=', '', args[grep('--upStream=', args)] )
     downStream <- sub( '--downStream=', '', args[grep('--downStream=', args)] )
-    Bins      <- sub( '--bins=', '', args[grep('--bins=', args)] )
-    assembly  <- sub( '--assembly=', '', args[grep('--assembly=', args)] )
-    bwFile    <- sub( '--bwFile=', '', args[grep('--bwFile=', args)])
-    Cores     <- sub( '--numCores=', '',args[grep('--numCores=',args)])
-    outName   <- sub( '--outName=', '',args[grep('--outName=',args)])
+    Bins       <- sub( '--bins=', '', args[grep('--bins=', args)] )
+    assembly   <- sub( '--assembly=', '', args[grep('--assembly=', args)] )
+    bwFile     <- sub( '--bwFile=', '', args[grep('--bwFile=', args)])
+    Cores      <- sub( '--numCores=', '',args[grep('--numCores=',args)])
+    outName    <- sub( '--outName=', '',args[grep('--outName=',args)])
+    tssMax     <- sub( '--tssMax=', '',args[grep('--tssMax=',args)])
 
 }
 
-#setwd("/projects/b1025/arw/analysis/delphine/termination_crispr")
-#regions  <- "tables/Pol2_CT_2Mio_HCT116_1087.filteredProteinCodingTx.rda"
-#assembly <- "hg19"
-#upStream <- 7500
-#downStream <- 7500
-#Window   <- 4000
-#Bins     <- 50
-#Type     <- "Tes"
-#bwFile <- "data_chipseq/Pol2_CT_2Mio_HCT116_1087.bw"
-#Cores <- 10
-#outName <-paste("tables/heatmaps", sub(".bw", "", basename(bwFile)), sep="/")
-    
+if (identical(tssMax,character(0))){
+    tssMax <- 0
+}else{
+    tssMax <- as.numeric(tssMax)
+}
+
 if (identical(Cores,character(0))){
    Cores <- 10
 }else{
@@ -146,6 +143,17 @@ if( length(grep(".bed$", regions)) > 0 ){
 
 if( Type=="Tss" ){
     print("model is Tss")
+    if ( tssMax== 1 ){
+        print("use max as Tss")
+        maxTss                                                      <- get(load(regions))
+        seqinfo(maxTss)                                             <- seqinfo(organism)[seqlevels(maxTss)]
+        print("fix start")
+        start(maxTss[paste(as.data.frame(maxTss)[,"strand"])=='+']) <- maxTss[paste(as.data.frame(maxTss)[,"strand"])=='+']$tssMaxStart
+        print("fix end")
+        end(maxTss[paste(as.data.frame(maxTss)[,"strand"])=='-'])   <- maxTss[paste(as.data.frame(maxTss)[,"strand"])=='-']$tssMaxStart
+        maxTss$length                                               <- width(maxTss)
+        Model                                                       <- maxTss
+    }
     Model.win      <- promoters(Model, upstream=upStream, downstream=downStream)
     Model.win$name <- Model.win$gene_id
     fname          <- sub("$", paste0("_", Type, "up", upStream, "down", downStream, ".rda"), outName)
@@ -196,18 +204,24 @@ matBin <- function(bw, model){
     mat <- do.call(rbind, mclapply(cov, as.numeric,mc.cores=Cores))
     x <- mat
     cov <- data.frame(x)
-    cat("bin the matrix\n")
-    window.cov <- function(row){
-        window <- as.integer(ncol(cov)/Bins)
-        window.coverage <- lapply(0:(window-1), function(jump)
-            rowMeans(row[as.numeric(jump*Bins+1):as.numeric((jump*Bins)+Bins)])
-            )
-        t(as.matrix(unlist(window.coverage)))
+    ## bin the matrix if needed
+    if( Bins > 0){
+        cat("bin the matrix\n")
+        window.cov <- function(row){
+            window <- as.integer(ncol(cov)/Bins)
+            window.coverage <- lapply(0:(window-1), function(jump)
+                rowMeans(row[as.numeric(jump*Bins+1):as.numeric((jump*Bins)+Bins)])
+                )
+            t(as.matrix(unlist(window.coverage)))
+        }
+        win <- mclapply(1:nrow(cov), function(i)
+            window.cov(cov[i,]),mc.cores=Cores)    
+        bin.mat <- do.call(rbind, mclapply(win, as.numeric, mc.cores=Cores))
+        df <- data.frame(bin.mat)
+    }else{
+        cat("binning is off\n")
+        df <- cov
     }
-    win <- mclapply(1:nrow(cov), function(i)
-        window.cov(cov[i,]),mc.cores=Cores)    
-    bin.mat <- do.call(rbind, mclapply(win, as.numeric, mc.cores=Cores))
-    df <- data.frame(bin.mat)
     rownames(df) <- model$name
     save(df,file=fname)
     print("done")
